8591c379cecaac80b08377744ff6f1ec10d66105
ian <aaaa0968877127@gmail.com>
2026-08-07 16:39:27 +0800

    刪除多於東西

--- Models/Permission.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-namespace backend.Models
-{
-	public class PermissionResponse
-	{
-		public string role_id  { get; set; } /* 權限代碼 */
-	}
-}
